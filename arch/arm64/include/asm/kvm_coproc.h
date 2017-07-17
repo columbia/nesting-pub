d95b663553c8b3b880dd5df7b3bb7c8d233828dd
Jintack Lim <jintack.lim@linaro.org>
2017-07-17 13:27:14 -0500

    KVM: arm64: Respect the virtual HCR_EL2.NV bit setting
    Forward traps due to HCR_EL2.NV bit to the virtual EL2 if they are not
    coming from the virtual EL2 and the virtual HCR_EL2.NV bit is set.
    
    This is for recursive nested virtualization.
    
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>
    
    Conflicts:
    	arch/arm64/kvm/sys_regs.c

--- arch/arm64/include/asm/kvm_coproc.h
+++ arch/arm64/include/asm/kvm_coproc.h
@@ -44,6 +44,7 @@ int kvm_handle_cp14_64(struct kvm_vcpu *vcpu, struct kvm_run *run);
 int kvm_handle_cp15_32(struct kvm_vcpu *vcpu, struct kvm_run *run);
 int kvm_handle_cp15_64(struct kvm_vcpu *vcpu, struct kvm_run *run);
 int kvm_handle_sys(struct kvm_vcpu *vcpu, struct kvm_run *run);
+bool forward_nv_traps(struct kvm_vcpu *vcpu);
 
 #define kvm_coproc_table_init kvm_sys_reg_table_init
 void kvm_sys_reg_table_init(void);
